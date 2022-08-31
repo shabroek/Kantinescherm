88b9cdfeaa49ae7da83d7eed7527838ec1807456
Sander van Broekhoven <s.vanbroekhoven@isah.com>
2022-08-31 16:00:55 +0200

    Initial commit

---
+++ JongBrabant.Kantinescherm/Data/MyDatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using DotNetCoreSqlDb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCoreSqlDb.Data
+{
+    public class MyDatabaseContext : DbContext
+    {
+        public MyDatabaseContext (DbContextOptions<MyDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Todo> Todo { get; set; }
+    }
+}
