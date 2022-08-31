c71b99fba96dddb2ab2c77791803bf3af93cab16
Sander van Broekhoven <s.vanbroekhoven@isah.com>
2022-08-31 17:19:20 +0200

    Add overview

--- JongBrabant.Kantinescherm/Controllers/HomeController.cs
+++ JongBrabant.Kantinescherm/Controllers/HomeController.cs
@@ -1,25 +1,12 @@
-﻿using System.Diagnostics;
-using JongBrabant.Kantinescherm.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace JongBrabant.Kantinescherm.Controllers
 {
-    public class HomeController : Controller
+    public class OverviewsController : Controller
     {
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ JongBrabant.Kantinescherm/Models/PriceList.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+
+namespace JongBrabant.Kantinescherm.Models;
+
+public class PriceList
+{
+    public List<GroupEntry> Groups { get; set; }
+}
