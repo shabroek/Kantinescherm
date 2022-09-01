260fda092b8c3ae191af28fa72e8087227b166ee
Sander van Broekhoven <s.vanbroekhoven@isah.com>
2022-09-01 22:34:24 +0200

    Update overview

---
+++ JongBrabant.Kantinescherm/Controllers/OverviewController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using JongBrabant.Kantinescherm.Data;
+using JongBrabant.Kantinescherm.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JongBrabant.Kantinescherm.Controllers
+{
+    public class OverviewController : Controller
+    {
+        private readonly PriceListContext _pricesContext;
+
+        public OverviewController(PriceListContext pricesContext)
+        {
+            _pricesContext = pricesContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var prices = await _pricesContext.Prices.Include(x => x.Group).ToListAsync();
+            var numberOfPrices = (int)Math.Floor(prices.Count / 3m);
+            var rest = prices.Count % 3;
+
+            var column1count = numberOfPrices + (rest > 0 ? 1 : 0);
+            var column2count = numberOfPrices + (rest > 1 ? 1 : 0);
+            var priceList = new PriceList
+            {
+                Columns1 = new PriceListColumn { Groups = prices.Take(column1count).GroupBy(x => x.Group).ToList() },
+                Columns2 = new PriceListColumn { Groups = prices.Skip(column1count).Take(column2count).GroupBy(x => x.Group).ToList() },
+                Columns3 = new PriceListColumn { Groups = prices.Skip(column1count).Skip(column2count).GroupBy(x => x.Group).ToList() }
+
+            };
+
+            return View(priceList);
+        }
+    }
+}
--- JongBrabant.Kantinescherm/Models/PriceList.cs
+++ JongBrabant.Kantinescherm/Models/PriceList.cs
@@ -1,8 +1,16 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace JongBrabant.Kantinescherm.Models;
 
 public class PriceList
 {
-    public List<GroupEntry> Groups { get; set; }
+    public PriceListColumn Columns1 { get; set; }
+    public PriceListColumn Columns2 { get; set; }
+    public PriceListColumn Columns3 { get; set; }
+}
+
+public class PriceListColumn
+{
+    public List<IGrouping<GroupEntry, PriceEntry>> Groups { get; set; }
 }
