ea680dfb9aa9d1fa8246651b1b1236d12e2843fa
Sander van Broekhoven <s.vanbroekhoven@isah.com>
2022-09-02 14:28:48 +0200

    Layout

--- JongBrabant.Kantinescherm/Controllers/OverviewController.cs
+++ JongBrabant.Kantinescherm/Controllers/OverviewController.cs
@@ -19,17 +19,17 @@ namespace JongBrabant.Kantinescherm.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var prices = await _pricesContext.Products.OrderBy(x => x.Group.Order).ThenBy(x => x.Order).Include(x => x.Group).ToListAsync();
-            var numberOfPrices = (int)Math.Floor(prices.Count / 3m);
-            var rest = prices.Count % 3;
+            var products = await _pricesContext.Products.OrderBy(x => x.Group.Order).ThenBy(x => x.Order).Include(x => x.Group).ToListAsync();
+            var numberOfProductsPerRow = (int)Math.Floor(products.Count / 3m);
+            var rest = products.Count % 3;
 
-            var column1count = numberOfPrices + (rest > 0 ? 1 : 0);
-            var column2count = numberOfPrices + (rest > 1 ? 1 : 0);
+            var column1Count = numberOfProductsPerRow + (rest > 0 ? 1 : 0);
+            var column2Count = numberOfProductsPerRow + (rest > 1 ? 1 : 0);
             var priceList = new PriceList
             {
-                Columns1 = new PriceListColumn { Groups = prices.Take(column1count).GroupBy(x => x.Group).ToList() },
-                Columns2 = new PriceListColumn { Groups = prices.Skip(column1count).Take(column2count).GroupBy(x => x.Group).ToList() },
-                Columns3 = new PriceListColumn { Groups = prices.Skip(column1count).Skip(column2count).GroupBy(x => x.Group).ToList() }
+                Columns1 = new PriceListColumn { Groups = products.Take(column1Count).GroupBy(x => x.Group).ToList() },
+                Columns2 = new PriceListColumn { Groups = products.Skip(column1Count).Take(column2Count).GroupBy(x => x.Group).ToList() },
+                Columns3 = new PriceListColumn { Groups = products.Skip(column1Count).Skip(column2Count).GroupBy(x => x.Group).ToList() }
 
             };
 
